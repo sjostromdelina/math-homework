1276182dc9c24d709dd24f32419648f487ee2ef2
sjostromdelina <sjostromdelina@gmail.com>
2025-03-10 10:52:11 +0000

    Add 5.py

---
+++ src/5.py
@@ -0,0 +1,7 @@
+def get_math_homework(level):
+    if level == 1:
+        return "print('Solve the following equation: x + 5 = 12')"
+    elif level == 2:
+        return "print('Find the area of a circle with radius 7')"
+    else:
+        return "print('Solve the quadratic equation ax^2 + bx + c = 0')"
