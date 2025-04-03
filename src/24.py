6fbc2efdb63df080a4f39ca7ad20e44db4097117
sjostromdelina <sjostromdelina@gmail.com>
2025-04-03 23:08:41 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,10 @@
+import math
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+result = factorial(5)
+print("Factorial of 5 is:", result)
