c470b8100c92e819a59478b87d154888d1277f86
sjostromdelina <sjostromdelina@gmail.com>
2025-05-20 10:46:09 +0000

    Add 38.py

---
+++ src/38.py
@@ -0,0 +1,16 @@
+def calculate_square_root(num):
+    """
+    Calculate the square root of a number.
+    
+    Parameters:
+        num (float): The number to find the square root of.
+        
+    Returns:
+        float: The square root of the input number.
+    """
+    return num ** 0.5
+
+# Example usage
+input_num = 16.0
+output_num = calculate_square_root(input_num)
+print(f"The square root of {input_num} is {output_num}")
